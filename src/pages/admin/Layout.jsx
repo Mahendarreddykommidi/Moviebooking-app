0d8b8350bdddc5506a2eb22c492545aefa7a50be
Mahenda reddy <mahendarreddykommidi82@gmail.com>
2025-06-20 12:38:32 +0530

    commits

--- src/pages/admin/Layout.jsx
+++ src/pages/admin/Layout.jsx
@@ -1,8 +1,7 @@
-
 import React from 'react'
 import AdminNavbar from "../../components/admin/AdminNavbar"
 
-import AdminSidebar from '../../components/admin/adminSidebar'
+import AdminSidebar from '../../components/admin/AdminSidebar'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
